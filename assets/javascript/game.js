b8cc84934ad7992fd6320beeed6559616f577562
Jcontratto <jmc22@hotmail.com>
2018-06-23 15:59:52 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,76 +17,76 @@ $(document).ready(function() {
     $("#gamesWon").text(totWins);
     $("#gamesLost").text(totLoss);
     $("#tallyNum").text(userValue);
-    
-        /* reset variables for a new game */
-        function newGame() {
-            goalScore = Math.floor((Math.random() * 102) + 19);
-            $("#goalNum").text(goalScore);
-                crystalVal1 = Math.floor(Math.random()*11+1);
-                crystalVal2 = Math.floor(Math.random()*11+1);
-                crystalVal3 = Math.floor(Math.random()*11+1);
-                crystalVal4 = Math.floor(Math.random()*11+1);
-                userAccum = 0;
-            $("#tallyNum").text(userValue);
-        };
-    
-        function gameWon() {
-            $("#gameStat").text("Smarty Pants!");
-            totWins++
-            $("#gamesWon").text(totWins);
-            newGame();
-        };
-     
-        function gameLost() {
-            $("#gameStat").text("Better Luck Next Time!");
-            totLoss++
-            $("#gamesLost").text(totLoss);
-            newGame();
-        };
-    
-        $("#buttonBlue").on("click", function() {
-            userValue = userValue + crystalVal1;
-            $("#tallyNum").text(userValue);
-            if (userValue === goalScore) {
-                gameWon();
-            }
-            else if (userValue > goalScore) {
-                gameLost();
-            }
-        });
-    
-        $("#buttonRed").on("click", function() {
-            userValue = userValue + crystalVal2;
-            $("#tallyNum").text(userValue);
-            if (userValue === goalScore) {
-                gameWon();
-            }
-            else if (userValue > goalScore) {
-                gameLost();
-            }
-        });
-    
-        $("#buttonYellow").on("click", function() {
-            userValue = userValue + crystalVal3;
-            $("#tallyNum").text(userValue);
-            if (userValue === goalScore) {
-                gameWon();
-            }
-            else if (userValue > goalScore) {
-                gameLost();
-            }
-        });
-    
-        $("#buttonGreen").on("click", function() {
-            userValue = userValue + crystalVal4;
-            $("#tallyNum").text(userValue);
-            if (userValue === goalScore) {
-                gameWon();
-            }
-            else if (userValue > goalScore) {
-                gameLost();
-            }
-        });
-        //need to have reset after you win or lose, also put the background image in on html/css 
+        
+    /* reset variables for a new game */
+    function newGame() {
+        goalScore = Math.floor((Math.random() * 102) + 19);
+        $("#goalNum").text(goalScore);
+            crystalVal1 = Math.floor(Math.random()*11+1);
+            crystalVal2 = Math.floor(Math.random()*11+1);
+            crystalVal3 = Math.floor(Math.random()*11+1);
+            crystalVal4 = Math.floor(Math.random()*11+1);
+            userAccum = 0;
+        $("#tallyNum").text(userValue);
+    };
+
+    function gameWon() {
+        $("#gameStat").text("Smarty Pants!");
+        totWins++
+        $("#gamesWon").text(totWins);
+        newGame();
+    };
+
+    function gameLost() {
+        $("#gameStat").text("Better Luck Next Time!");
+        totLoss++
+        $("#gamesLost").text(totLoss);
+        newGame();
+    };
+
+    $("#buttonBlue").on("click", function() {
+        userValue = userValue + crystalVal1;
+        $("#tallyNum").text(userValue);
+        if (userValue === goalScore) {
+            gameWon();
+        }
+        else if (userValue > goalScore) {
+            gameLost();
+        }
+        
     });
- 
+
+    $("#buttonRed").on("click", function() {
+        userValue = userValue + crystalVal2;
+        $("#tallyNum").text(userValue);
+        if (userValue === goalScore) {
+            gameWon();
+        }
+        else if (userValue > goalScore) {
+            gameLost();
+        }
+    });
+
+    $("#buttonYellow").on("click", function() {
+        userValue = userValue + crystalVal3;
+        $("#tallyNum").text(userValue);
+        if (userValue === goalScore) {
+            gameWon();
+        }
+        else if (userValue > goalScore) {
+            gameLost();
+        }
+    });
+
+    $("#buttonGreen").on("click", function() {
+        userValue = userValue + crystalVal4;
+        $("#tallyNum").text(userValue);
+        if (userValue === goalScore) {
+            gameWon();
+        }
+        else if (userValue > goalScore) {
+            gameLost();
+        }
+    });
+    //need to have reset after you win or lose, keeps adding onto to same score 
+});
